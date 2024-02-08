c4c1930c44c506ceaf255a184e844653c48bd52e
Sam Luong <81666239+Sangculu@users.noreply.github.com>
2024-02-08 11:10:49 +0000

    Upload completed deep.py

--- 1-Conditionals/deep/deep.py
+++ 1-Conditionals/deep/deep.py
@@ -1,6 +1,9 @@
 def main():
-    realAnswer = input()
-    if realAnswer == 42:
+    x = input(
+        "What is the real anwswer to the Great Question of Life, the Universe and Everything."
+    )
+    y = x.lower().replace("-", "").replace(" ", "")
+    if y == "fortytwo" or y == "42":
         print("Yes")
     else:
         print("No")

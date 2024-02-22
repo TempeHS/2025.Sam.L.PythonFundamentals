d2bafdfb76ae18a81556b13fa6e02e54abe28d5d
Sam Luong <81666239+Sangculu@users.noreply.github.com>
2024-02-22 11:46:45 +0000

    Upload completed outdated.py

--- 3-Exceptions/outdated/outdated.py
+++ 3-Exceptions/outdated/outdated.py
@@ -1,14 +1,13 @@
-str = input("Valid date: ")
 months = {
-    "January": "1",
-    "February": "2",
-    "March": "3",
-    "April": "4",
-    "May": "5",
-    "June": "6",
-    "July": "7",
-    "August": "8",
-    "September": "9",
+    "January": "01",
+    "February": "02",
+    "March": "03",
+    "April": "04",
+    "May": "05",
+    "June": "06",
+    "July": "07",
+    "August": "08",
+    "September": "09",
     "October": "10",
     "November": "11",
     "December": "12",
@@ -16,49 +15,56 @@ months = {
 
 date = ""
 while True:
-    if str[0:2].isnumeric():
+    str = input("Valid date: ")
+    # Check if the date is in the format "Month Day, Year"
+    if str[0:2].isalpha():
         iso_date = str.replace(",", "")
         try:
-            months, day, year = iso_date.split(" ")
+            month, day, year = iso_date.split(" ")
         except ValueError:
             print("Enter a valid date:")
-    else:
-        try:
-            month, day, year = str.split("/")
-        except ValueError:
-            str = input("Enter a valid date: ")
             continue
+    # Check if the date is in the format "Day/Month/Year"
+    elif str.count("/") == 2:
+        try:
+            day, month, year = str.split("/")
+        except ValueError:
+            print("Enter a valid date:")
+            continue
+    else:
+        print("Enter a valid date:")
+        continue
     # Day check: 0 < day < 32
     if int(day) > 0 and int(day) < 32:
-        date += day
+        date += year
         date += "-"
     else:
         date = ""
-        str = input("Enter a valid date: ")
+        print("Enter a valid date:")
         continue
     # Month check:
     #   Non-Numeric: Check dict, if not, fails
     #   Numeric: Check 0 < month < 13
     if not month.isnumeric():
-        if month in dict:
-            date += dict[month]
+        if month in months:
+            date += months[month]
             date += "-"
         else:
             date = ""
-            str = input("Enter a valid date: ")
+            print("Enter a valid date:")
             continue
     elif int(month) < 13 and int(month) > 0:
-        date += month
-        date += "-"
+        date += "{:02d}-".format(int(month))
     else:
         date = ""
-        str = input("Enter a valid date: ")
+        print("Enter a valid date:")
         continue
-    # Year check: year > 0
-    if int(year) > 0:
-        date += year
+    # Day check: 0 < day < 32
+    if int(day) > 0 and int(day) < 32:
+        date += "{:02d}".format(int(day))
         break
     else:
-        str = input("Enter a valid date: ")
+        date = ""
+        print("Enter a valid date:")
         continue
 print(date)

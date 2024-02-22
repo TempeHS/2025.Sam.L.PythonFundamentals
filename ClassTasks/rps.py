6f9c7d7ffa11b55ec467f7c322de7863bb1bbbd8
Sam Luong <81666239+Sangculu@users.noreply.github.com>
2024-02-22 00:45:34 +0000

    Upload completed rps.py

--- ClassTasks/rps.py
+++ ClassTasks/rps.py
@@ -1,10 +1,28 @@
 import random
 
-playerchoice = input("Enter your decision: ").lower
+prompt = True
 
+while prompt:
+    options = ["paper", "rock", "scissor"]
+    player = None
 
-def computer_choice(x):
-    computer_choice = ["paper" or "p", "rock" or "r", "scissor" or "s"]
-    x = computer_choice
-    print(random.choice(x))
-    return x
+    computer_choice = random.choice(options)
+    player = input("Enter your choice (Rock, Paper, Scissor): ").lower()
+
+    print(f"Player Choice: {player}")
+    print(f"Computer choice: {computer_choice}")
+
+    if player == computer_choice:
+        print("Tie")
+    elif player == "rock" and computer_choice == "scissor":
+        print("You win")
+    elif player == "paper" and computer_choice == "rock":
+        print("You Win")
+    elif player == "scissor" and computer_choice == "paper":
+        print("You win")
+    else:
+        print("Computer win")
+    play_again = input("You want to player again? (y/n):").lower()
+    if not play_again == "y":
+        prompt = False
+print("GOOD GAME, GG")

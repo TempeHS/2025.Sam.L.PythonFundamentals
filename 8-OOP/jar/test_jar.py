9c64d5e9df424b5cc0a179cd25c6915f25034654
Sam Luong <81666239+Sangculu@users.noreply.github.com>
2024-05-10 23:57:08 +0000

    Upload completed test_jar.py

--- 8-OOP/jar/test_jar.py
+++ 8-OOP/jar/test_jar.py
@@ -1,1 +1,28 @@
+from jar import Jar
+import pytest
 
+
+def main():
+    test_init()
+    test_str()
+
+
+def test_init():
+    jar = Jar()
+    assert jar.capacity == 12
+    assert jar.size == 0
+
+
+def test_str():
+    jar = Jar()
+    assert str(jar) == ""
+    jar.deposit(1)
+    assert str(jar) == "🍪"
+    jar.deposit(5)
+    assert str(jar) == "🍪🍪🍪🍪🍪🍪"
+
+
+# def test_deposit():
+
+
+# def test_withdraw(): ...

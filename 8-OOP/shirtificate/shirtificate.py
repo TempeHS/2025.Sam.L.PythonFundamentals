cbb87a0a2157f984018db7442f2e62f4629517b6
Sam Luong <81666239+Sangculu@users.noreply.github.com>
2024-05-13 03:47:38 +0000

    Upload completed shirtificate.py

--- 8-OOP/shirtificate/shirtificate.py
+++ 8-OOP/shirtificate/shirtificate.py
@@ -3,29 +3,24 @@ from fpdf import FPDF
 
 class PDF(FPDF):
     def header(self):
-        self.set_font("helvetica", "", 50)
-        self.cell(0, 60, "CS50 Shirtificate", align="C")
-        self.image(
-            "/workspaces/2025.Sam.L.PythonFundamentals/8-OOP/shirtificate/shirtificate.png",
-            10,
-            70,
-            190,
-        )
-        self.set_font_size(30)
+        self.image("./shirtificate.png", 10, 70, 190)
+        self.set_font("helvetica", "", 48)
+        self.cell(0, 57, "CS50 Shirtificate", align="C")
         self.ln(20)
 
 
 def main():
-    name = input("Your name: ")
+    name = input("Name: ")
     shirt(name)
 
 
 def shirt(s):
     pdf = PDF()
-    pdf.add_page(orientation="P", format="A4")
-    pdf.output("shirtificate.pdf")
+    pdf.add_page(orientation="portrait", format="a4")
+    pdf.set_font("helvetica", size=24)
     pdf.set_text_color(255, 255, 255)
     pdf.cell(0, 213, f"{s} took CS50", align="C")
+    pdf.output("shirtificate.pdf")
 
 
 if __name__ == "__main__":
